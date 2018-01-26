c5164f977fa03d9a60c855157ceb880bdcf7ae72
gndlovu <gladwell_n@live.com>
2018-01-26 06:35:54 +0200

    Initial commit

---
+++ views/home.blade.php
@@ -0,0 +1,18 @@
+@extends('laravel_api::layouts.app')
+
+@section('content')
+
+    <div class="flex-center position-ref full-height">
+        <div class="content">
+            <div class="title m-b-md">
+                Task Management Tool - Backend
+            </div>
+
+            <div class="links">
+                <a href="https://laravel.com/docs">Angular front-end</a>
+                <a href="https://laracasts.com">Laravel back-end</a>
+            </div>
+        </div>
+    </div>
+
+@endsection
